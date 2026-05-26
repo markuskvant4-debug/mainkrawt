5ab6460848fd0fa96b149cb46b45e0784d28a4d3
markuskvant4-debug <markuskvant4@gmail.com>
2026-05-26 18:29:49 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-# Скачиваем и запускаем playit в фоне
+# Скачиваем официальный агент playit прямо при старте
 curl -SsL https://playit-cloud.github.io/playit-agent-built/playit-linux-amd64~0.17.1 -o playit
 chmod +x playit
+
+# Запускаем туннель playit в фоновом режиме, используя твою секретную переменную
 ./playit --secret $PLAYIT_SECRET &
 
-# Запускаем Майнкрафт (он будет главным процессом)
-exec java -Xmx512M -Xms256M -jar server.jar --nogui --port 10000
+# Запускаем сам сервер Майнкрафт на порту 10000 с оптимизацией памяти под бесплатный тариф
+exec java -Xmx400M -Xms256M -jar server.jar --nogui --port 10000
