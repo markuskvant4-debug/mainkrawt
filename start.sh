3ed676b7bf3e6f2297fc36cc9d4dc99d7d8c802e
markuskvant4-debug <markuskvant4@gmail.com>
2026-05-26 18:15:38 +0300

    Refactor start.sh to simplify playit agent launch

--- start.sh
+++ start.sh
@@ -1,15 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
-# Запуск playit-агента в фоновом режиме
-if [ -z "$PLAYIT_SECRET" ]; then
-    echo "ВНИМАНИЕ: PLAYIT_SECRET не задан в настройках Environment!"
-else
-    echo "Запуск туннеля Playit.gg..."
-    curl -SsL https://playit-cloud.github.io/playit-agent-built/playit-linux-amd64~0.17.1 -o playit
-    chmod +x playit
-    ./playit --secret $PLAYIT_SECRET &
-fi
+# Скачиваем и запускаем playit в фоне
+curl -SsL https://playit-cloud.github.io/playit-agent-built/playit-linux-amd64~0.17.1 -o playit
+chmod +x playit
+./playit --secret $PLAYIT_SECRET &
 
-# Запуск самого сервера Майнкрафт
-echo "Запуск сервера Minecraft 1.12.2..."
+# Запускаем Майнкрафт (он будет главным процессом)
 exec java -Xmx512M -Xms256M -jar server.jar --nogui --port 10000
